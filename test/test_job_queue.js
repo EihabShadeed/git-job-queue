e1655fe0ab566629dd90f3ef31ccc9c0faecd694
Ali Ismael <aismael@paypal.com>
2015-08-12 15:52:44 -0700

    version 1 release

--- test/test_job_queue.js
+++ test/test_job_queue.js
@@ -5,27 +5,27 @@
 
 var assert = require('assert');
 
-
-'use strict';
-
-
 var path = require('path');
 
+var exec = require("child_process").exec;
 
-var job_queue_1 = require("../lib/index")({
-    instanceId: "job_queue_1",
+
+var job_queue_1 = require("../lib/index");
+
+var repoConfig = {
+    repository: {
+        git_cmd_path: "c:\\Program Files (x86)\\Git\\bin\\git.exe",
+        git_repository_uri: "git@github.com:gaaiatinc/async-job-queue.git",
+        clone_dir: "./TestRepo",
+        git_remote_name: "origin",
+        git_branch: "master"
+    },
+    instanceId: "git_job_queue",
     persistent: false,
     maxSize: 1000,
     heartbeatMillis: 4000,
     consoleLog: true
-}, dummyExecutor);
-var job_queue_2 = require("../lib/index")({
-    instanceId: "job_queue_2",
-    persistent: false,
-    maxSize: 1000,
-    heartbeatMillis: 1000,
-    consoleLog: true
-}, dummyExecutor);
+};
 
 
 function dummyExecutor(aJob) {
@@ -57,8 +57,9 @@ describe('job-queue', function () {
         //
         //});
 
-        job_queue_1.init();
-        job_queue_2.init(done);
+        job_queue_1.init(repoConfig)
+            .then(done)
+            .finally(done);
     });
 
 
@@ -87,63 +88,110 @@ describe('job-queue', function () {
     /**
      *
      */
-    describe('JobQueue Performance test', function () {
+    describe('GitJobQueue Performance test', function () {
 
-        it('should run without error', function (done) {
+        //it("original code", function (done) {
+        //
+        //    var remoteName = "origin";
+        //    var repoURI = "git@github.com:gaaiatinc/git-job-queue.git";
+        //
+        //    (function checkRemote() {
+        //        exec("git remote -v", {cwd: "./TestRepo"}, function (gitErr, output) {
+        //            console.log("output:", output);
+        //
+        //            var elems, idx;
+        //            if (gitErr) {
+        //                logger.warn("Could not check for repo consistency:", gitErr);
+        //                return done();
+        //            } else {
+        //                if (output) {
+        //                    elems = output.trim().split(/[\s]+/);
+        //                    for (idx = 0; (idx + 2) < elems.length; idx += 3) {
+        //                        if (elems[idx + 2] === "(fetch)" && elems[idx] === remoteName && elems[idx + 1] === repoURI) {
+        //                            return done();
+        //                        }
+        //                    }
+        //                }
+        //                console.log("Could not find a matching remote name:", remoteName);
+        //                return done();
+        //            }
+        //        });
+        //    }());
+        //});
 
-            var numJobsCreated = 0;
-            var numJobsExecuted = 0;
+        it('should clean git repository without error', function (done) {
 
-            ++numJobsCreated;
-            job_queue_1.pushJob("Job_1_q_1").then(function () {
-                numJobsExecuted--;
-            });
-
-            ++numJobsCreated;
-            job_queue_1.pushJob("Job_2_q_1").then(function () {
-                numJobsExecuted--;
-            });
-
-            setTimeout(function () {
-                ++numJobsCreated;
-                job_queue_1.pushJob("q_1_" + new Date().toString()).then(function () {
-                    numJobsExecuted--;
+            job_queue_1.cleanRepositoryFolder()
+                .then(function () {
+                    console.log("repository is wipedout:");
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    done();
                 });
-            }, 1000);
+
+        });
 
 
-            ++numJobsCreated;
-            job_queue_2.pushJob("Job_2_q_2").then(function () {
-                numJobsExecuted--;
-            });
+        it('should clone git repository without error', function (done) {
 
-            ++numJobsCreated;
-            job_queue_2.pushJob("Job_3_q_2").then(function () {
-                numJobsExecuted--;
-            });
-
-            for (var i = 0; i < 100000; i++) {
-                ++numJobsCreated;
-                job_queue_2.pushJob("q_2_" + new Date().toString()).then(function () {
-                    numJobsExecuted--;
+            job_queue_1.cloneRepository()
+                .then(function () {
+                    console.log("repository is cloned:");
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    done();
                 });
-            }
 
-            ++numJobsCreated;
-            job_queue_2.pushJob("Job_1_q_2").then(function () {
-                numJobsExecuted--;
-                console.log(">p>p>p completed job Job_1_q_2");
-            });
+        });
 
-            setTimeout(function () {
-                job_queue_1.shutdown();
-                job_queue_2.shutdown();
+        it('should test git repository without error', function (done) {
 
-                console.log("number of jobs created:", numJobsCreated);
-                console.log("number of jobs executed:", numJobsExecuted);
-                done();
-            }, 15000);
+            job_queue_1.testRepository()
+                .then(function () {
+                    console.log("repository is ok:");
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    done();
+                });
 
+        });
+
+        it('should schedule a git command without error', function (done) {
+
+            job_queue_1.scheduleGitCommand(["log", "-5"])
+                .then(function (results) {
+                    console.log("repository is ok, last few logs:", results);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    done();
+                });
+
+        });
+
+        it('should list modified upstream files without error', function (done) {
+
+            job_queue_1.listModifiedUpstreamFiles()
+                .then(function (results) {
+                    console.log("repository is ok, modified upstream files:", results);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                })
+                .finally(function () {
+                    done();
+                });
 
         });
     });
